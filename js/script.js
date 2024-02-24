242bad88490e314c4f61f2435161153494ceeb1e
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2024-02-24 13:54:35 +0100

    update

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let menu = document.getElementById('menu');
+let display = document.getElementById('display');
+let closeMenu = document.getElementById('closeMenu')
+
+function displayMenu(){
+  menu.style.display = 'block';
+}
+
+
+
+
+
+
+
+
+
+
+/*
+document.addEventListener('DOMContentLoaded', function() {
+    alert('ok');
+});
+*/
